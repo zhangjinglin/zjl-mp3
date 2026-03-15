6d4b586904000570783767cc747da71642a84a16
Zhang Jinglin <zhangjinglin@gmail.com>
2026-03-15 11:02:43 +0800

    Initial Commit

---
+++ zjl-mp3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  zjl-mp3
+//
+//  Created by ZhangJinglin on 2026/3/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ zjl-mp3/zjl_mp3App.swift
@@ -0,0 +1,17 @@
+//
+//  zjl_mp3App.swift
+//  zjl-mp3
+//
+//  Created by ZhangJinglin on 2026/3/15.
+//
+
+import SwiftUI
+
+@main
+struct zjl_mp3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
